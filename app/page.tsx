978250ffe272c33681388b16f2395d2bfdee5f48
Arnav Angarkar <arnav.angarkar20@gmail.com>
2025-10-29 02:24:16 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import StoryCarousel from "@/components/story-carousel"
+import Header from "@/components/header"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <StoryCarousel />
+    </main>
+  )
+}
